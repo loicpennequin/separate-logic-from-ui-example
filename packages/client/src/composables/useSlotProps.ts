5cacce9ba0913faa73a5bba24154bfa16ea5607b
loicpennequin <lo.pennequin@gmail.com>
2023-01-13 23:09:47 +0100

    removed unused cide

--- packages/client/src/composables/useSlotProps.ts
+++
@@ -1,4 +0,0 @@
-import type { AnyObject } from '@daria/shared';
-
-// used when passing props to a slot, for convenience (the consumer doesn't need to use .value in the template)
-export const useSlotProps = <T extends AnyObject>(obj: T) => reactive(obj);
